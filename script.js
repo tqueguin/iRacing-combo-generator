83bac05af2a6f051cde4e6194eefb43f7c5ef084
tanguy.queguineur <tanguy.queguineur@student.vinci.be>
2022-05-20 05:12:48 +0200

    multiclass

--- script.js
+++ script.js
@@ -9,3 +9,14 @@ function randomCombo() {
   document.getElementById("car").innerHTML = car;
   document.getElementById("timeOfDay").innerHTML = time;
 }
+
+function multiclassCombo() {
+  const track = tracks[Math.floor(Math.random() * tracks.length)];
+  const car = cars[Math.floor(Math.random() * cars.length)].car_name;
+  const car2 = cars[Math.floor(Math.random() * cars.length)].car_name;
+  const car3= cars[Math.floor(Math.random() * cars.length)].car_name;
+  const time = times[Math.floor(Math.random() * times.length)];
+  document.getElementById("track").innerHTML = track.track_name + " - " + track.config_name;
+  document.getElementById("car").innerHTML = car + car2 + car3;
+  document.getElementById("timeOfDay").innerHTML = time;
+}
