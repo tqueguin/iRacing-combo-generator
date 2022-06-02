a30d10ed854951e1cdbce9e4617366507ba66fb8
tanguy.queguineur <tanguy.queguineur@student.vinci.be>
2022-06-02 18:49:32 +0200

    add type selector

--- script.js
+++ script.js
@@ -1,22 +1,65 @@
-const times = ['Morning', 'Noon', 'Afternoon', 'Sunset', 'Night'];
+const times = ['Sunrise', 'Morning', 'Noon', 'Afternoon', 'Late afternoon', 'Sunset', 'Night'];
 
 
 function randomCombo() {
-  const track = tracks[Math.floor(Math.random() * tracks.length)];
-  const car = cars[Math.floor(Math.random() * cars.length)].car_name;
+  const track = selectTrack();
+  const car = selectCar();
   const time = times[Math.floor(Math.random() * times.length)];
+  
   document.getElementById("track").innerHTML = track.track_name + " - " + track.config_name;
-  document.getElementById("car").innerHTML = car;
+  document.getElementById("car").appendChild(document.createElement("li").appendChild(document.createTextNode(car.car_name)));
+  document.getElementById("car").app = car.car_name;
   document.getElementById("timeOfDay").innerHTML = time;
+  document.getElementById('addCar').hidden = false;
 }
 
-function multiclassCombo() {
-  const track = tracks[Math.floor(Math.random() * tracks.length)];
-  const car = cars[Math.floor(Math.random() * cars.length)].car_name;
-  const car2 = cars[Math.floor(Math.random() * cars.length)].car_name;
-  const car3= cars[Math.floor(Math.random() * cars.length)].car_name;
-  const time = times[Math.floor(Math.random() * times.length)];
-  document.getElementById("track").innerHTML = track.track_name + " - " + track.config_name;
-  document.getElementById("car").innerHTML = car + car2 + car3;
-  document.getElementById("timeOfDay").innerHTML = time;
+function addCar() {
+  const car = selectCar();
+  document.getElementById("car").appendChild(document.createElement("li").appendChild(document.createTextNode(car.car_name)));
+}
+
+
+
+function selectTrack() {
+  let accepted_track_types = [];
+  if (document.getElementById('roadTracks').checked) {
+    accepted_track_types.push("road");
+  }
+  if (document.getElementById('ovalTracks').checked) {
+    accepted_track_types.push("oval");
+  }
+  let track = null;
+  while (track === null) {
+    let randomTrack = tracks[Math.floor(Math.random() * tracks.length)];
+    if (accepted_track_types.includes(randomTrack.track_types[0].track_type)) {
+      track = randomTrack;
+    }
+  }
+
+  return track;
+}
+
+
+function selectCar() {
+  let accepted_car_types = [];
+  if (document.getElementById('roadCars').checked) {
+    accepted_car_types.push("road");
+  }
+  if (document.getElementById('ovalCars').checked) {
+    accepted_car_types.push("oval");
+  }
+
+
+  let car = null;
+  while (car === null) {
+    let randomCar = cars[Math.floor(Math.random() * cars.length)];
+    for (var i = 0; i < randomCar.car_types.length; i++) {
+      if (accepted_car_types.includes(randomCar.car_types[i].car_type)) {
+        car = randomCar;
+        continue;
+      }
+    }
+  }
+
+  return car;
 }
